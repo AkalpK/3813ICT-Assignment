11ece14a2cea4348f7d28b2f73ea4a4e23eb5f43
AkalpK <87978228+AkalpK@users.noreply.github.com>
2021-10-13 19:05:22 +1000

    Added MongoDB to server-side
    Successfully sends data from database to browser

--- server/socket.js
+++ server/socket.js
@@ -3,11 +3,6 @@ module.exports = {
         io.on('connection', (socket) => {
             // WHEN A CONNECTION REQUEST COMES IN OUTPUT TO THE SERVER CONSOLE
             console.log('User connection on port ' + PORT + ' : ' + socket.id);
-
-            // When a message comes in emit it back to all sockets wit hthe message.
-            socket.on('message', (message)=>{
-                io.emit('message',message);
-            })
             
             socket.on('databaseRequest', () => {
                 io.emit('database', database);
